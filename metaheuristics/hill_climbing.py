49e50c7908131caf7005448b519148398948f793
Martin Noya <martinnoya@gmail.com>
2018-04-11 20:32:52 -0300

    Hill Climbling random restart

--- metaheuristics/hill_climbing.py
+++ metaheuristics/hill_climbing.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+MAX_RETRIES = 10
+
 def surroundings(center, radius, domains):
     """ The surroundings of a `center` is a list of new centers, all equal to the center except for
     one value that has been increased or decreased by `radius`.
@@ -9,7 +11,7 @@ def surroundings(center, radius, domains):
                if center[i] - radius >= domains[i][0] and center[i] + radius <= domains[i][1]
            ]
 
-def hill_climbing(problem, steps=100, delta=1, initial=None):
+def hill_climbing(problem, steps=100, delta=1, max_retries=MAX_RETRIES, initial=None):
     """ Hill climbing optimization implemented as a generator function. 
     """
     current = initial or problem.randomElement()
@@ -22,7 +24,12 @@ def hill_climbing(problem, steps=100, delta=1, initial=None):
         if problem.compareEvaluations(lastEval, current[1]) > 0:
             lastEval = current[1]
         else:
-            break # local optimum has been reached
+            if max_retries > 0:
+                print("RESTARTED")
+                yield from hill_climbing(problem, steps, delta, max_retries-1)
+                break
+            else:
+                break # local optimum has been reached
         yield current
 
 def test1(problem=None):
@@ -30,4 +37,4 @@ def test1(problem=None):
         from .test_problems import hello_world
         problem = hello_world()
     for step in hill_climbing(problem, steps=10000):
-        print(step, ''.join(map(chr, step[0])))
+        print(step)
