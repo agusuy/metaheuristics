63e5397e3bc67aa7d1219eb8136b4ac27654e3e6
Leonardo Val <leonardo.val@creatartis.com>
2018-04-11 17:57:23 -0300

    Fixed Schaffer N2 implementation + Turns out sin^2 is sin squared.

--- metaheuristics/test_problems.py
+++ metaheuristics/test_problems.py
@@ -12,10 +12,12 @@ def hello_world(target_str="Hello world!"):
         objective = lambda chars: sum(abs(c - t) for (c, t) in zip(chars, target_chars))
     )
 
-# From <https://en.wikipedia.org/wiki/Test_functions_for_optimization> ##############################
+# References:
+# + [Test functions for optimization @ Wikipedia](https://en.wikipedia.org/wiki/Test_functions_for_optimization)
+# + [Test functions and datasets @ Virtual Library of Simulation Experiments](https://www.sfu.ca/~ssurjano/optimization.html)
 
 def __schaffer_N2__(elem):
     (x,y) = elem
-    return 0.5 + (sin(sin(x*x - y*y)) - 0.5)/((1 + 0.001 * (x*x + y*y)) ** 2)
+    return 0.5 + (sin(x*x - y*y) ** 2 - 0.5)/((1 + 0.001 * (x*x + y*y)) ** 2)
 
 SCHAFFER_N2 = OptimizationProblem(domains= ((-100,+100),)*2, objective=__schaffer_N2__)
