db2eee71c10e473043e186a6f8adf26759e76593
Rodrigo Suárez <rodrigosrz12@gmail.com>
2018-04-17 08:06:00 -0300

    Ejercicio 1.4
    Integrantes: Marcelo De León, Diego Romero, Nicolás Rodriguez, Rodrigo Suárez.
    
    - Se implementó Simulated Annealing.
    - Se implementaron 2 problemas mas de la página dada.
    - Se creo archivo test.py y se cambiaron configuraciones para poder ejecutar desde el todos los problemas con todas las algoritmos.

---
+++ metaheuristics/__init__.py
@@ -0,0 +1,5 @@
+from . import hill_climbing
+from . import problem
+from . import test_problems
+from . import simulated_annealing
+from . import random_restart_hill_climbing
--- metaheuristics/hill_climbing.py
+++ metaheuristics/hill_climbing.py
@@ -5,12 +5,12 @@ def surroundings(center, radius, domains):
     one value that has been increased or decreased by `radius`.
     """
     return [center[0:i] + (center[i] + d,) + center[i + 1:]
-               for i in range(len(center)) for d in (-radius, +radius) 
-               if center[i] - radius >= domains[i][0] and center[i] + radius <= domains[i][1]
+               for i in range(len(center)) for d in (-radius, +radius)
+               if center[i] + d >= domains[i][0] and center[i] + d <= domains[i][1]
            ]
 
 def hill_climbing(problem, steps=100, delta=1, initial=None):
-    """ Hill climbing optimization implemented as a generator function. 
+    """ Hill climbing optimization implemented as a generator function.
     """
     current = initial or problem.randomElement()
     lastEval = problem.objective(current)
@@ -25,9 +25,26 @@ def hill_climbing(problem, steps=100, delta=1, initial=None):
             break # local optimum has been reached
         yield current
 
-def test1(problem=None):
-    if not problem:
-        from .test_problems import hello_world
-        problem = hello_world()
-    for step in hill_climbing(problem, steps=10000):
-        print(step, ''.join(map(chr, step[0])))
+def schaffer_n2():
+    from .test_problems import SCHAFFER_N2
+    problem = SCHAFFER_N2
+    finalStep = list(hill_climbing(problem, steps=10000))[-1]
+    print(finalStep)
+
+def bukin_n6():
+    from .test_problems import BUKIN_N6
+    problem = BUKIN_N6
+    finalStep = list(hill_climbing(problem, steps=100000))[-1]
+    print(finalStep)
+
+def sum_squares():
+    from .test_problems import SUM_SQUARES
+    problem = SUM_SQUARES
+    finalStep = list(hill_climbing(problem, steps=10000))[-1]
+    print(finalStep)
+
+def hello_world():
+    from .test_problems import hello_world
+    problem = hello_world()
+    finalStep = list(hill_climbing(problem, steps=10000))[-1]
+    print("".join(map(chr,finalStep[0])), finalStep[1])
