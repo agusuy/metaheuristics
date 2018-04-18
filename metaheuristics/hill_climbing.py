b6759792cefad8928637c6f5bd7cbeff79d77b88
Unknown <fglf18@gmail.com>
2018-04-18 11:38:52 -0300

    Cambios minimos

--- metaheuristics/hill_climbing.py
+++ metaheuristics/hill_climbing.py
@@ -6,7 +6,7 @@ def surroundings(center, radius, domains):
     """
     return [center[0:i] + (center[i] + d,) + center[i + 1:]
                for i in range(len(center)) for d in (-radius, +radius) 
-               if center[i] - radius >= domains[i][0] and center[i] + radius <= domains[i][1]
+               if center[i] + d >= domains[i][0] and center[i] + d <= domains[i][1]
            ]
 
 def hill_climbing(problem, steps=100, delta=1, initial=None):
@@ -18,8 +18,7 @@ def hill_climbing(problem, steps=100, delta=1, initial=None):
     yield current
     for step in range(steps):
         nexts = problem.evaluated(surroundings(current[0], delta, problem.domains))
-        if(len(nexts) >= 1):
-            current = nexts[0]
+        current = nexts[0]
         if problem.compareEvaluations(lastEval, current[1]) > 0:
             lastEval = current[1]
         else:
